a429d5a1db59d24cf6f4bb57b510ade74bc883c1
nsc <nsc@qq.com>
2025-01-13 10:01:45 +0800

    update header

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -4,7 +4,7 @@ import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Bars3Icon, BugAntIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, BugAntIcon, BuildingOfficeIcon, ArrowPathIcon, CakeIcon, CurrencyDollarIcon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
 
@@ -20,10 +20,30 @@ export const menuLinks: HeaderMenuLink[] = [
     href: "/",
   },
 
-  {
+/*   {
     label: "Debug Contracts",
     href: "/debug",
     icon: <BugAntIcon className="h-4 w-4" />,
+  }, */
+  {
+    label: "mining",
+    href: "/mining",
+    icon: <BuildingOfficeIcon className="h-4 w-4" />,
+  },
+  {
+    label: "swap",
+    href: "/swap",
+    icon: <ArrowPathIcon className="h-4 w-4" />,
+  },
+  {
+    label: "invite",
+    href: "/invite",
+    icon: <CakeIcon className="h-4 w-4" />,
+  },
+  {
+    label: "assets",
+    href: "/assets",
+    icon: <CurrencyDollarIcon className="h-4 w-4" />,
   },
 ];
 
